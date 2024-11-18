b9095608e3814613a830dd3c20dc8d37de4552d9
stornquist <stornquist@microsoft.com>
2024-11-18 13:52:26 -0800

    working

---
+++ aks_cluster/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.95"
+    }
+  }
+}
+
+provider "azurerm" {
+  environment = "usgovernment"
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "usgovvirginia"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name = "example-network"
+  location = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space = ["10.0.0.1/24"]
+}
+
+# resource "azurerm_subnet" "example" {
+#   name                 = "internal"
+#   resource_group_name  = azurerm_resource_group.example.name
+#   virtual_network_name = azurerm_virtual_network.example.name
+# }
+
+# resource "azurerm_network_security_group" "example" {
+#   name                = "example-nsg"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+# }
--- lease_lock/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  #backend "local" {}
-  backend "azurerm" {
-    resource_group_name  = "tfstate"
-    storage_account_name = "tfstate3x57z"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
