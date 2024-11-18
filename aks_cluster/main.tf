44c8cfb58f4f24355ddadfb9ad3ea29546ec5246
stornquist <stornquist@microsoft.com>
2024-11-18 13:55:28 -0800

    storage account

--- aks_cluster/main.tf
+++ aks_cluster/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_virtual_network" "example" {
   name = "example-network"
   location = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  address_space = ["10.0.0.1/24"]
+  address_space = ["10.0.0.0/24"]
 }
 
 # resource "azurerm_subnet" "example" {
